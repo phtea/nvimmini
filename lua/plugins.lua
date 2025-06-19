8c1a5f0185866d98c45c5f5bb823f23d42c3f96e
spushmenkov <spushmenkov@phoenixit.ru>
2025-06-20 02:33:44 +0300

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,9 +13,16 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			-- Telescope
+			require('telescope').setup{
+				pickers = {
+					find_files = { theme = "ivy", },
+					live_grep = { theme = "ivy", },
+				},
+			}
+
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
-		end
+		end,
 	},
 }
