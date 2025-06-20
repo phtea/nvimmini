5ab29034d9bb25eedb49805e7e87107f0f537a29
spushmenkov <spushmenkov@phoenixit.ru>
2025-06-20 12:47:50 +0300

    works solid

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,6 @@
 -- Telescope
 return {
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.6",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     local telescope = require("telescope")
@@ -20,10 +19,12 @@ return {
     pcall(vim.keymap.del, "x", "gra")
     pcall(vim.keymap.del, "n", "gra")
     pcall(vim.keymap.del, "n", "grn")
+    pcall(vim.keymap.del, "n", "gd")
 
     -- Keymaps
     vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
     vim.keymap.set("n", "gr", builtin.lsp_references, { desc = "Find references" })
+    vim.keymap.set("n", "gd", builtin.lsp_definitions, { desc = "Find definitions" })
   end,
 }
