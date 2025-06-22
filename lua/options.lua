1940a50818cb8af4d0c088de10e04b83b68c1c34
tea <tea>
2025-06-22 03:32:03 +0300

    persistent undo

--- lua/options.lua
+++ lua/options.lua
@@ -11,3 +11,15 @@ vim.opt.hlsearch = true
 -- Grep
 vim.opt.grepprg = "rg --vimgrep --no-heading --smart-case"
 vim.opt.grepformat = "%f:%l:%c:%m"
+
+-- Enable persistent undo
+vim.opt.undofile = true
+
+-- Set undo directory
+local undodir = vim.fn.stdpath("data") .. "/undo"
+vim.opt.undodir = undodir
+
+-- Auto-create the undo directory if it doesn't exist
+if vim.fn.isdirectory(undodir) == 0 then
+  vim.fn.mkdir(undodir, "p")
+end
