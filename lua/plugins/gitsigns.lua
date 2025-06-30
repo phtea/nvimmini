b38eeb4a9db19dea9aad5ed8752efbdadad9d563
tea <tea>
2025-06-30 22:30:33 +0300

    gitsigns keybinds

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,18 @@
 return {
     'lewis6991/gitsigns.nvim',
     config = function()
-	require('gitsigns').setup()
+	require('gitsigns').setup {
+	    on_attach = function(bufnr)
+		local gs = package.loaded.gitsigns
+
+		local opts = {buffer = bufnr, noremap = true, silent = true}
+
+		-- Navigation
+		vim.keymap.set('n', '<leader>gn', gs.next_hunk, opts)
+		vim.keymap.set('n', '<leader>gp', gs.prev_hunk, opts)
+		vim.keymap.set('n', '<leader>grh', gs.reset_hunk, opts)
+		vim.keymap.set('n', '<leader>grb', gs.reset_buffer, opts)
+	    end
+	}
     end
 }
