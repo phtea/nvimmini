20f1fec889fd2aa85181284ae83d6a7f2f1979b0
spushmenkov <spushmenkov@phoenixit.ru>
2025-07-01 18:06:08 +0300

    added keybind for preview hunk and removed reset buffer (rare use case, when i need it i can type it manually)

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -10,8 +10,10 @@ return {
 		-- Navigation
 		vim.keymap.set('n', '<leader>gn', gs.next_hunk, opts)
 		vim.keymap.set('n', '<leader>gp', gs.prev_hunk, opts)
-		vim.keymap.set('n', '<leader>grh', gs.reset_hunk, opts)
-		vim.keymap.set('n', '<leader>grb', gs.reset_buffer, opts)
+		-- Reset hunk
+		vim.keymap.set('n', '<leader>gr', gs.reset_hunk, opts)
+		-- Preview hunk
+		vim.keymap.set('n', '<leader>gp', gs.preview_hunk, opts)
 	    end
 	}
     end
