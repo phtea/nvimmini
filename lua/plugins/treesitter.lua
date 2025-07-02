7c3096753439bf031c932af20e8f5571a9b32959
spushmenkov <spushmenkov@phoenixit.ru>
2025-07-02 20:38:07 +0300

    treesitter additional_vim_regex = true

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,7 +10,7 @@ return {
 		},
 		highlight = {
 		    enable = true,
-		    additional_vim_regex_highlighting = false,
+		    additional_vim_regex_highlighting = true,
 		},
 		indent = {
 		    enable = true,
